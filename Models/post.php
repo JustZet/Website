eec4b8b6fba66c47a35fb16777595d5041511259
zetCodex <fsflorentin@gmail.com>
2022-01-13 16:25:27 +0200

    Models for Api

---
+++ Models/post.php
@@ -0,0 +1,34 @@
+<?php 
+  class Post {
+    // DB stuff
+    private $conn;
+    private $table = 'website';
+
+    // Post Properties
+    public $ID;
+    public $FirstName;
+    public $LastName;
+    public $Adress;
+    public $PhoneNumber;
+    public $Password;
+    public $FileName;
+
+    // Constructor with DB
+    public function __construct($db) {
+      $this->conn = $db;
+    }
+
+    // Get Posts
+    public function read() {
+      // Create query
+      $query = 'SELECT * FROM website';
+      
+      // Prepare statement
+      $stmt = $this->conn->prepare($query);
+
+      // Execute query
+      $stmt->execute();
+
+      return $stmt;
+    }
+  }
