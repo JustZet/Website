61ee2b51359c28be04fcee8a1f12cf9825111476
zetCodex <fsflorentin@gmail.com>
2022-01-14 23:37:49 +0200

    Now the file really call a api in body of web page

--- fetch.js
+++ fetch.js
@@ -1,12 +1,21 @@
-let request = new Request("Api/read.php");
+function fetchData() {
+    let request = new Request("Api/read.php");
+    fetch(request) 
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data) => {
+        console.log(data);
+        const html = data.data.map(users => {
+            return `<p> Adress: ${users.First_Name} </p>`;
+        })
+        .join("");
+        document.querySelector("#users")
+        .insertAdjacentHTML('afterbegin', html)
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}
 
-fetch(request)
-.then((resp) => {
-    return resp.json();
-})
-.then((data) => {
-    console.log(data);
-})
-.catch((err) => {
-    console.log(err);
-});
+fetchData();
