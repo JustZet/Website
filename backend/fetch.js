a0f221e71063dfca084ecddd1b315485b43994e5
zetCodex <fsflorentin@gmail.com>
2022-01-17 03:51:18 +0200

    Just added this item in backend/

---
+++ backend/fetch.js
@@ -0,0 +1,24 @@
+let request = new Request("Api/users.php");
+async function fetchData() {
+    await fetch(request) 
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data) => {
+        console.log(data);
+        const html = data.data.map(users => {
+            return `<div>
+            <p>User: ${users.First_Name} </p>
+            <p>LastName: ${users.Last_Name} </p>
+            </div>`;
+        })
+        .join("");
+        document.querySelector("#users")
+        .insertAdjacentHTML('afterbegin', html)
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}
+
+fetchData();
