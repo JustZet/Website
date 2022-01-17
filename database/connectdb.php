8f137b7541ed14dafb6459e0ef698840389027dc
zetCodex <fsflorentin@gmail.com>
2022-01-17 03:54:10 +0200

    Modifyed name from connect.php in connectdb.php

---
+++ database/connectdb.php
@@ -0,0 +1,13 @@
+<?php
+// Db params
+$host = "localhost";
+$username = "root";
+$db_name = "index";
+$password = "";
+// Connect with database
+$con = mysqli_connect($host, $username, $password, $db_name);
+
+// Check connection
+if($con === false) {
+    die("ERROR: Could not connect" . mysqli_connect_error());
+}
