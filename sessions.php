6cd9d77915775bd64ca8b0cb24c0f8b5c59342b3
zetCodex <fsflorentin@gmail.com>
2022-01-17 03:47:50 +0200

    Modifyed bar organization of items

--- sessions.php
+++ sessions.php
@@ -6,8 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About</title>
     <link rel="stylesheet" href="css/index.css" type="text/css">
-    <link rel="stylesheet" media="screen and (max-width: 1050px)" href="css/widerscreen.css">
-    <link rel="stylesheet" media="screen and (max-width: 600px)" href="css/smallerscreen.css">
     <link href="https://fonts.googleapis.com/css2?family=Material+Icons"
       rel="stylesheet">
 </head>
@@ -18,12 +16,13 @@
       <li><a href="home.html"><span class="material-icons">
         home
         </span></a></li>
-      <li class="active"><a href="sessions.php">Sessions</a></li>
-      <li id="signup"><a href="signup.html">Sign up</a></li>
-      <li id="signin"><a href="signin.html">Sign in</a></li>
+      <li id="signup-bar"><a href="signup.html">Sign up</a></li>
+      <li id="signin-bar"><a href="signin.php">Sign in</a></li>
+      <li id="sessions-bar"><a href="sessions.php">Sessions</a></li>
     </b>
   </ul>
 </div>
+
 <div class="container info">
     <?php
     session_start();
