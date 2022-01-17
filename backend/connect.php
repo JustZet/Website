b7cae98bf74e434e2b31f3bd59e7cad15980a349
zetCodex <fsflorentin@gmail.com>
2022-01-18 01:47:44 +0200

    Modifyed row 17 - From signin.php... to ../signin.php...

--- backend/connect.php
+++ backend/connect.php
@@ -9,12 +9,11 @@ if(isset($_POST['submit'])) {
 $sql = "SELECT * FROM website WHERE PhoneNumber='".$Phone."' AND Password='".$Password."'LIMIT 1";
   $result     = mysqli_query($con, $sql);
   $rows       = mysqli_num_rows($result);
+  $newURL     = ('../home.html');
   if($rows == 1) {
-    echo 'You have corectly connected!';
-    $newURL = ('../home.html');
     header('Location: '.$newURL);
     exit();
   }else {
-    header('Location: signin.php?info=error');
+    header('Location: ../signin.php?info=error');
   };
 };
