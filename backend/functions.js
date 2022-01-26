611ddae2b1bd89e3c5eea3c450ad845e96d5b9f0
zetCodex <fsflorentin@gmail.com>
2022-01-26 23:46:40 +0200

    Added the function for background paralax from background.js and added in functions.js because is a function, right?

--- backend/functions.js
+++ backend/functions.js
@@ -1,3 +1,4 @@
+// Implementation navbar function
 function displayNavbar() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
@@ -6,7 +7,7 @@ function displayNavbar() {
       x.style.display = "block";
     }
   }
-  
+// Open and close nav function
 function openNav() {
     document.getElementById("Sidepanel").style.width = "250px";
 }
@@ -14,3 +15,18 @@ function openNav() {
 function closeNav() {
     document.getElementById("Sidepanel").style.width = "0";
 }
+// Background paralax function
+let stars = document.getElementById("stars");
+let moon = document.getElementById("moon");
+let mntnb = document.getElementById("montain");
+let text = document.getElementById("text");
+let mntnf = document.getElementById("front");
+window.addEventListener("scroll", function() {
+  let value = window.scrollY;
+
+  stars.style.left = value * 0.35 + 'px';
+  moon.style.top = value * 0.7 + 'px';
+  mntnb.style.top = value * 0.5 + 'px';
+  mntnf.style.top = value * 0 + 'px';
+  text.style.marginLeft = value * -1 + 'px';
+})
